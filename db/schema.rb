357a92ccffe779c7b0a712f82577d4ffdc879654
alejoescobar <alejoescobac@gmail.com>
2015-04-30 12:19:19 -0500

    Add company model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430170321) do
+ActiveRecord::Schema.define(version: 20150430171244) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "companies", ["user_id"], name: "index_companies_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
